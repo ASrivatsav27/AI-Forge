963eff3e8508636a7e84ab8217dca5bf897e1688
ASrivatsav27 <srivatsava47@gmail.com>
2026-07-29 20:23:20 +0530

    feat: add Better Auth with Prisma, email, GitHub and Google OAuth

--- backend/index.ts
+++ backend/index.ts
@@ -3,16 +3,13 @@ import app from "./src/app.js";
 import {Server,Socket} from "socket.io"
 import { createServer } from "http"
 
+
 const server = createServer(app)
 const io = new Server(server)
 
 io.on("connection", (socket: Socket) => {
     console.log("User connected",socket.id)
 })
-
-
-
-
 
 server.listen(8000, () => {
     console.log("Server is running on port 8000")
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express"
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth.js";
 const app = express()
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+app.use(express.json())
 
 export default app
