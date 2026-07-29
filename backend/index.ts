984810a044f4daf6e619960e095e075311dacf47
ASrivatsav27 <srivatsava47@gmail.com>
2026-07-29 17:53:22 +0530

    feat: add Socket.IO server integration

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,19 @@
 
 import app from "./src/app.js";
+import {Server,Socket} from "socket.io"
+import { createServer } from "http"
 
-app.listen(8000, () => {
+const server = createServer(app)
+const io = new Server(server)
+
+io.on("connection", (socket: Socket) => {
+    console.log("User connected",socket.id)
+})
+
+
+
+
+
+server.listen(8000, () => {
     console.log("Server is running on port 8000")
 })
