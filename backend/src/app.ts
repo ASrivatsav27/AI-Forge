f5aa7c70507eadb186f12f509ebd0989ba7c6572
ASrivatsav27 <srivatsava47@gmail.com>
2026-09-25 11:18:26 +0530

    Server Health Check Route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import { errorHandler } from "./middleware/error-handler.js";
 import { serve } from "inngest/express";
 import { inngest } from "./config/inngest.js"
 import { functions } from "./workflows/index.js";
-
+import type { Request,Response } from "express";
 
 const app = express()
 
@@ -18,6 +18,8 @@ app.use(cors({origin: "http://localhost:3000",credentials: true,}));
 // that history easily exceed Express's 100kb default.
 app.use(express.json({ limit: "50mb" }));
 
+
+
 app.use("/api/inngest", serve({client: inngest,functions}));
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
@@ -25,4 +27,12 @@ app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use("/project", projectRouter);
 
 app.use(errorHandler);
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({
+        "message":"Server is running"
+    })
+})
+
+
 export default app
